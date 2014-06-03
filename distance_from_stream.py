ab6e1a062b80e22ab45831cb9cdcfb6d8de83f44
Richard Sharp <richpsharp@gmail.com>
2014-06-03 11:08:17 -0700

    Tracking the new distance from stream script.

---
+++ distance_from_stream.py
@@ -0,0 +1,13 @@
+from invest_natcap.routing import routing_utils
+
+if __name__ == '__main__':
+    mg_parameters = {
+        'dem_filename': '',
+        'lulc_filename': '',
+        'convert_from_lulc_codes': [],
+        'convert_to_lulc_code': 0,
+        'pixels_per_step_to_convert': 100,
+        'number_of_steps': 10,
+        'temporary_file_directory': '',
+        'output_file_directory': '',
+    }
