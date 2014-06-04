228b80adff2dd2cefa71779b9165633702477db3
Richard Sharp <richpsharp@gmail.com>
2014-06-04 12:10:20 -0700

    Update issue #1, built the mask of the distance transform that'll be used to select the order in which pixels are converted from the stream.

--- distance_from_stream.py
+++ distance_from_stream.py
@@ -2,6 +2,9 @@ import os
 import hashlib
 import pickle
 
+import gdal
+import numpy
+
 from invest_natcap.routing import routing_utils
 from invest_natcap import raster_utils
 
@@ -58,12 +61,12 @@ def initialize_simulation(parameters):
             parameters['temporary_file_directory'], 'distance_from_stream.tif')
         
         print 'resolving filling pits'
-        dem_pit_filled_uri =  os.path.join(
+        dem_pit_filled_uri = os.path.join(
             parameters['temporary_file_directory'], 'pit_filled_dem.tif')
         routing_utils.fill_pits(parameters['dem_filename'], dem_pit_filled_uri)
         
         print 'resolving plateaus'
-        dem_plateau_resolved_uri =  os.path.join(
+        dem_plateau_resolved_uri = os.path.join(
             parameters['temporary_file_directory'], 'plateau_resolved_dem.tif')
         routing_utils.resolve_flat_regions_for_drainage(
             dem_pit_filled_uri, dem_plateau_resolved_uri)
@@ -94,17 +97,39 @@ def initialize_simulation(parameters):
 
  
 def step_land_change(parameters):
-    for step_number in xrange(parameters['number_of_steps']):
-        print step_number
+    conversion_priority_filename = os.path.join(
+        parameters['temporary_file_directory'], 'conversion_priority.tif')
+    conversion_nodata = -1
+    conversion_pixel_size = raster_utils.get_cell_size_from_uri(
+        parameters['lulc_filename'])
+    lulc_nodata = raster_utils.get_nodata_from_uri(parameters['lulc_filename'])
+        
+    def valid_distance(distance, lulc):
+        conversion_array = numpy.empty(distance.shape, dtype=numpy.float32)
+        conversion_array[:] = conversion_nodata
+        for convert_code in parameters['convert_from_lulc_codes']:
+            mask = (lulc == convert_code)
+            conversion_array[mask] = distance[mask]
+    
+        return conversion_array
+    
+    print 'building the prioritization from stream raster'
+    raster_utils.vectorize_datasets(
+        [parameters['distance_from_stream_filename'], parameters['lulc_filename']],
+        valid_distance, conversion_priority_filename, gdal.GDT_Float32,
+        conversion_nodata, conversion_pixel_size, 'intersection',
+        dataset_to_align_index=0, vectorize_op=False)
+
 
 
 if __name__ == '__main__':
     PARAMETERS = {
         #'dem_filename': 'C:/Users/rich/Dropbox/unilever_data/mg_dem_90f/w001001.adf',
+        #'lulc_filename': 'C:/Users/rich/Dropbox/unilever_data/lulc_2008.tif',
         'dem_filename': "C:/InVEST_dev39_3_0_1 [6d541e569a05]_x86/Base_Data/Freshwater/dem/w001001.adf",
-        'flow_accumulation_threshold_for_streams': 1000, 
-        'lulc_filename': 'C:/Users/rich/Dropbox/unilever_data/lulc_2008.tif',
-        'convert_from_lulc_codes': [2],
+        'lulc_filename': 'C:/InVEST_dev39_3_0_1 [6d541e569a05]_x86/Base_Data/Terrestrial/landuse_90/w001001.adf',
+        'flow_accumulation_threshold_for_streams': 1000,
+        'convert_from_lulc_codes': range(49, 67) + [95, 98],
         'convert_to_lulc_code': 18,
         'pixels_per_step_to_convert': 100,
         'number_of_steps': 10,
