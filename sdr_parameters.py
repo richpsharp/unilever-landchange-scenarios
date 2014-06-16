790bdf879a7bd0d792d76c9f6aa2eb2d9303a3ce
Richard Sharp <richpsharp@gmail.com>
2014-06-16 15:16:39 -0700

    Update issue #1
    Added mg arguments

--- sdr_parameters.py
+++ sdr_parameters.py
@@ -6,23 +6,41 @@ InVEST version: dev81:3.0.1 [e65614c6b30b]
 
 import invest_natcap.sdr.sdr
 
-args = {
-        u'biophysical_table_uri': u'C:/Users/rich/Dropbox/Unilever_data_from_Stacie/Input_Iowa_national/biophysical_coeffs_Iowa_Unilever_national.csv',
-        u'dem_uri': u"C:/Users/rich/Dropbox/Unilever_data_from_Stacie/Input_Iowa_national/DEM_SRTM_Iowa_HUC8.tif",
-        u'erodibility_uri': u"C:/Users/rich/Dropbox/Unilever_data_from_Stacie/Input_Iowa_national/erodibility_STATSGO_Iowa_HUC8.tif",
-        u'erosivity_uri': u"C:/Users/rich/Dropbox/Unilever_data_from_Stacie/Input_Iowa_national/erosivity_Iowa_HUC8.tif",
-        u'ic_0_param': u'0.5',
-        u'k_param': u'2',
-        u'landuse_uri': u"C:/Users/rich/Dropbox/Unilever_data_from_Stacie/Input_Iowa_national/LULC_NLCD_2006_Iowa_HUC8.tif",
-        u'sdr_max': u'0.8',
-        u'threshold_flow_accumulation': u'1000',
-        u'watersheds_uri': u"C:/Users/rich/Dropbox/Unilever_data_from_Stacie/Input_Iowa_national/HUC8_Iowa_intersect_dissolve.shp",
-        u'workspace_dir': u'C:/Users/rich/Documents/Iowa_national',
-        u'suffix': 'test',
+iowa_args = {
+    u'biophysical_table_uri': u'C:/Users/rich/Dropbox/Unilever_data_from_Stacie/Input_Iowa_national/biophysical_coeffs_Iowa_Unilever_national.csv',
+    u'dem_uri': u"C:/Users/rich/Dropbox/Unilever_data_from_Stacie/Input_Iowa_national/DEM_SRTM_Iowa_HUC8.tif",
+    u'erodibility_uri': u"C:/Users/rich/Dropbox/Unilever_data_from_Stacie/Input_Iowa_national/erodibility_STATSGO_Iowa_HUC8.tif",
+    u'erosivity_uri': u"C:/Users/rich/Dropbox/Unilever_data_from_Stacie/Input_Iowa_national/erosivity_Iowa_HUC8.tif",
+    u'ic_0_param': u'0.5',
+    u'k_param': u'2',
+    u'landuse_uri': u"C:/Users/rich/Dropbox/Unilever_data_from_Stacie/Input_Iowa_national/LULC_NLCD_2006_Iowa_HUC8.tif",
+    u'sdr_max': u'0.8',
+    u'threshold_flow_accumulation': u'1000',
+    u'watersheds_uri': u"C:/Users/rich/Dropbox/Unilever_data_from_Stacie/Input_Iowa_national/HUC8_Iowa_intersect_dissolve.shp",
+    u'workspace_dir': u'C:/Users/rich/Documents/unilever_output/Iowa_national',
+    u'suffix': 'iowa',
 }
 
-print 'preparing sdr'
-args['_prepare'] = invest_natcap.sdr.sdr._prepare(**args)
+mg_args = {
+    u'biophysical_table_uri': u"C:/Users/rich/Dropbox/Unilever_data_from_Stacie/Input_MatoGrosso_global/biophysical_coeffs_Brazil_Unilever.csv",
+    u'dem_uri': u"C:/Users/rich/Dropbox/Unilever_data_from_Stacie/Input_MatoGrosso_global/DEM_SRTM_MT_filled.tif",
+    u'erodibility_uri': u"C:/Users/rich/Dropbox/Unilever_data_from_Stacie/Input_MatoGrosso_global/erodibility_MT.tif",
+    u'erosivity_uri': u"C:/Users/rich/Dropbox/Unilever_data_from_Stacie/Input_MatoGrosso_global/erosivity_MT.tif",
+    u'ic_0_param': u'0.5',
+    u'k_param': u'2',
+    u'landuse_uri': u"C:/Users/rich/Dropbox/Unilever_data_from_Stacie/Input_MatoGrosso_global/LULC_MCD12Q1_2012_MT.tif",
+    u'sdr_max': u'0.8',
+    u'threshold_flow_accumulation': u'1000',
+    u'watersheds_uri': u"C:/Users/rich/Dropbox/Unilever_data_from_Stacie/Input_MatoGrosso_global/Mato_Grosso.shp",
+    u'workspace_dir': u'C:/Users/rich/Documents/unilever_output/Mato_Grosso_global/',
+    u'suffix': 'mato_grosso',
+}
 
-print 'calling sdr'
-invest_natcap.sdr.sdr.execute(args)
+
+for args in [mg_args]:
+
+    print 'preparing sdr'
+    args['_prepare'] = invest_natcap.sdr.sdr._prepare(**args)
+
+    print 'calling sdr'
+    invest_natcap.sdr.sdr.execute(args)
